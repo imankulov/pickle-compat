01619ebec4aada74ab8c085cc3eba39f4852d81b
Roman Imankulov <roman.imankulov@gmail.com>
2020-05-29 00:02:05 +0100

    Define pickle_compat's __all__

--- pickle_compat/__init__.py
+++ pickle_compat/__init__.py
@@ -1,5 +1,12 @@
 import pkg_resources
 
-from pickle_compat.compat import patch, unpatch  # noqa: F401
+from pickle_compat.compat import patch, unpatch
 
 __version__ = pkg_resources.get_distribution("pickle-compat").version
+
+
+__all__ = [
+    "patch",
+    "unpatch",
+    "__version__",
+]
