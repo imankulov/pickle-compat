b651481ed3df3b4969de215f9d966feb2192a28e
Mehaei <40540969+Mehaei@users.noreply.github.com>
2020-12-29 03:18:05 +0800

    Add compatibility with py3.7 (#3)
    Co-authored-by: shuaiwei <shuaiwei.meng@stratifyd.cn>

--- pickle_compat/compat.py
+++ pickle_compat/compat.py
@@ -14,21 +14,21 @@ if sys.version_info.major == 3:
     compat_loads = partial(pickle._loads, encoding=DEFAULT_ENCODING)  # type: ignore
 
     # Backward-compatible dump and dumps, that use the second version of the protocol.
-    def compat_dump(obj, file, protocol=None, fix_imports=True, buffer_callback=None):
+    def compat_dump(obj, file, protocol=None, fix_imports=True, **kwargs):
         return pickle._dump(
             obj,
             file,
             protocol=DEFAULT_PROTOCOL,
             fix_imports=fix_imports,
-            buffer_callback=buffer_callback,
+            **kwargs
         )
 
-    def compat_dumps(obj, protocol=None, fix_imports=True, buffer_callback=None):
+    def compat_dumps(obj, protocol=None, fix_imports=True, **kwargs):
         return pickle._dumps(
             obj,
             protocol=DEFAULT_PROTOCOL,
             fix_imports=fix_imports,
-            buffer_callback=buffer_callback,
+            **kwargs
         )
 
     # Backward-compatible versions of functions and classes
